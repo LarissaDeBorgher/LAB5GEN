d924564f0a69993bb209ecb33cf7e4cb1e1f714a
LarissaDeBorgher <r0839981@student.thomasmore.be>
2022-10-21 14:08:08 +0200

    Feat: Add messages router

---
+++ routes/messages.js
@@ -0,0 +1,11 @@
+// require express router
+const express = require('express');
+const router = express.Router();
+// require message controller
+const messageController = require('../controllers/messages');
+
+router.get("/", messageController.getAll);
+
+router.post("/", messageController.create);
+
+ module.exports = router;
